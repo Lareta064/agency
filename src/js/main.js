779df769993bcd21c360b19363e6b3a2b477226e
Lareta064 <lareta064@gmail.com>
2023-06-05 20:32:56 +0300

    ашч

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,33 @@ document.addEventListener('DOMContentLoaded', function () {
 	/*=======  показать/скрыть мобильное меню =========== */
 	const headerMenu = document.querySelector('#header-menu');
 	const menuToggle = document.querySelector('#menu-toggle');
-
-	menuToggle.addEventListener('click', function () {
+	const bodyEl = document.body;
+	/*=== показать/скрыть моб меню по клику по бургеру */
+	menuToggle.addEventListener('click', ()=>{
 		if (!menuToggle.classList.contains('active')) {
 			headerMenu.classList.add('open');
-			this.classList.add('active');
+			menuToggle.classList.add('active');
+			body.classList.add('lock');
 		} else {
 			headerMenu.classList.remove('open');
-			this.classList.remove('active');
+			menuToggle.classList.remove('active');
+			body.classList.remove('lock');
 		}
 
+	});
+	/*=== закрыть моб меню по клику по нему */
+	headerMenu.addEventListener('click', ()=>{
+		headerMenu.classList.remove('open');
+		menuToggle.classList.remove('active');
+		body.classList.remove('lock');
+	});
+	/*=== закрыть моб меню при ресайзе экрана */
+	window.addEventListener('resize', ()=>{
+		if (window.innerWidth != window.innerHeight){
+			headerMenu.classList.remove('open');
+			menuToggle.classList.remove('active');
+			body.classList.remove('lock');
+		}
+		
 	})
 });
